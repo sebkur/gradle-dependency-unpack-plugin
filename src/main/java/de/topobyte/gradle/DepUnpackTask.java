de88760bb45317dfb5be7abf4eedb37d3a5107ab
Sebastian Kürten <sebastian@topobyte.de>
2017-11-02 12:37:12 +0100

    Unzipping non-jsweet jars to 'build/unpackedJars'

--- src/main/java/de/topobyte/gradle/DepUnpackTask.java
+++ src/main/java/de/topobyte/gradle/DepUnpackTask.java
@@ -1,7 +1,19 @@
 package de.topobyte.gradle;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
@@ -57,6 +69,10 @@ public class DepUnpackTask extends AbstractDepUnpackTask
 					.println(String.format("%s:%s:%s", group, module, version));
 		});
 
+		File buildDir = project.getBuildDir();
+		File output = new File(buildDir, "unpackedJars");
+		output.mkdirs();
+
 		Set<ResolvedArtifact> artifacts = configuration
 				.getResolvedConfiguration().getResolvedArtifacts();
 		for (ResolvedArtifact artifact : artifacts) {
@@ -67,18 +83,52 @@ public class DepUnpackTask extends AbstractDepUnpackTask
 				String group = mci.getGroup();
 				String module = mci.getModule();
 				String version = mci.getVersion();
-				String extension = artifact.getExtension();
-				printInfo(group, module, version, extension, artifact);
-				resolveSource(group, module, version);
+				if (group.equals("org.jsweet")
+						&& module.equals("jsweet-core")) {
+					System.out.println(String.format("JSweet core: %s:%s:%s",
+							group, module, version));
+					continue;
+				}
+				if (isCandyJar(artifact)) {
+					System.out.println(String.format("Candy: %s:%s:%s", group,
+							module, version));
+					continue;
+				}
+				System.out.println(String.format("Normal: %s:%s:%s", group,
+						module, version));
+
+				List<ResolvedArtifactResult> sources = resolveSource(group,
+						module, version);
+				if (sources.isEmpty()) {
+					System.out.println("no source found");
+				}
+				for (ResolvedArtifactResult resolvedSourceResult : sources) {
+					printInfo(group, module, version, resolvedSourceResult);
+					try {
+						unpack(resolvedSourceResult.getFile(), output);
+					} catch (IOException e) {
+						System.out.println("Error while unpacking "
+								+ resolvedSourceResult.getFile());
+						e.printStackTrace();
+					}
+				}
 			}
 		}
 	}
 
-	private void printInfo(String group, String module, String version,
-			String extension, ResolvedArtifact artifact)
+	private boolean isCandyJar(ResolvedArtifact artifact)
 	{
-		System.out.println(String.format("%s:%s:%s %s %s", group, module,
-				version, extension, artifact.getFile()));
+		boolean candyMetadataFound = false;
+		File file = artifact.getFile();
+		try {
+			ZipFile zip = new ZipFile(file);
+			ZipEntry entry = zip.getEntry("META-INF/candy-metadata.json");
+			candyMetadataFound = entry != null;
+			zip.close();
+		} catch (IOException e) {
+			return false;
+		}
+		return candyMetadataFound;
 	}
 
 	private void printInfo(String group, String module, String version,
@@ -88,7 +138,8 @@ public class DepUnpackTask extends AbstractDepUnpackTask
 				resolvedSourceResult.getFile()));
 	}
 
-	private void resolveSource(String group, String module, String version)
+	private List<ResolvedArtifactResult> resolveSource(String group,
+			String module, String version)
 	{
 		Project project = getProject();
 		DependencyHandler handler = project.getDependencies();
@@ -103,16 +154,55 @@ public class DepUnpackTask extends AbstractDepUnpackTask
 				.forComponents(id).withArtifacts(JvmLibrary.class, classes);
 		ArtifactResolutionResult result = query.execute();
 
+		List<ResolvedArtifactResult> all = new ArrayList<>();
 		for (ComponentArtifactsResult resolved : result
 				.getResolvedComponents()) {
 			for (ArtifactResult sourceResult : resolved
 					.getArtifacts(SourcesArtifact.class)) {
 				if (sourceResult instanceof ResolvedArtifactResult) {
 					ResolvedArtifactResult resolvedSourceResult = (ResolvedArtifactResult) sourceResult;
-					printInfo(group, module, version, resolvedSourceResult);
+					all.add(resolvedSourceResult);
 				}
 			}
 		}
+
+		return all;
+	}
+
+	private void unpack(File zip, File output) throws ZipException, IOException
+	{
+		InputStream fis = new FileInputStream(zip);
+		ZipInputStream zis = new ZipInputStream(fis);
+
+		ZipEntry entry = zis.getNextEntry();
+		while (entry != null) {
+			String fileName = entry.getName();
+			File file = new File(output, fileName);
+
+			System.out.println("unzip: " + file.getAbsoluteFile());
+			if (entry.isDirectory()) {
+				file.mkdirs();
+			} else {
+				file.getParentFile().mkdirs();
+				unpack(zis, file);
+			}
+
+			entry = zis.getNextEntry();
+		}
+
+		zis.close();
+		fis.close();
+	}
+
+	private void unpack(ZipInputStream zis, File file) throws IOException
+	{
+		byte[] buffer = new byte[1024];
+		OutputStream fos = new FileOutputStream(file);
+		int len;
+		while ((len = zis.read(buffer)) > 0) {
+			fos.write(buffer, 0, len);
+		}
+		fos.close();
 	}
 
 }
